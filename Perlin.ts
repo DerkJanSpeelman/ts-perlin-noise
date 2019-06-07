4cfe6805c32783615227d754230fef27f63b05df
Derk Jan Speelman <derkjanspeelman@gmail.com>
2019-06-07 14:27:59 +0200

    Final commit

--- Perlin.ts
+++ Perlin.ts
@@ -1,8 +1,15 @@
 export class Perlin {
 
-    private static perm: number[] = [ 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 ];
+    private perm: number[];
+    private p: number[];
 
-    private static permutation = (perm: number[]): number[] => {
+    constructor() {
+
+        this.perm = [ 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 ];
+        this.p = this.permutation(this.perm);
+    }
+
+    private permutation = (perm: number[]): number[] => {
 
         let p: number[] = perm;
 
@@ -14,25 +21,10 @@ export class Perlin {
         return p;
     }
 
-    public static noise = (x: number, y: number = 0, z: number = 0): number => {
+    public noise = (x: number, y: number = 0, z: number = 0): number => {
 
-        const perm: number[] = Perlin.perm;
-        const p: number[] = Perlin.permutation(perm);
-
-        const fade: (t: number) => number = Perlin.fade;
-
-        const lerp: (
-            t: number,
-            a: number,
-            b: number
-        ) => number = Perlin.lerp;
-
-        const grad: (
-            hash: number,
-            x:    number,
-            y:    number,
-            z:    number
-        ) => number = Perlin.grad;
+        const perm: number[] = this.perm;
+        const p: number[] = this.permutation(perm);
 
         let X: number = Math.floor(x) & 255,
             Y: number = Math.floor(y) & 255,
@@ -42,9 +34,9 @@ export class Perlin {
         y -= Math.floor(y);
         z -= Math.floor(z);
 
-        let u: number = fade(x),
-            v: number = fade(y),
-            w: number = fade(z);
+        let u: number = this.fade(x),
+            v: number = this.fade(y),
+            w: number = this.fade(z);
 
         let A:  number = p[X    ] + Y,
             AA: number = p[A    ] + Z,
@@ -52,28 +44,28 @@ export class Perlin {
             B:  number = p[X + 1] + Y,
             BA: number = p[B    ] + Z,
             BB: number = p[B + 1] + Z;
-        
-        return lerp(w, lerp(v, lerp(u, grad(p[AA    ], x    , y    , z     ),
-                                       grad(p[BA    ], x - 1, y    , z     )),
-                               lerp(u, grad(p[AB    ], x    , y - 1, z     ),
-                                       grad(p[BB    ], x - 1, y - 1, z     ))),
-                       lerp(v, lerp(u, grad(p[AA + 1], x    , y    , z - 1 ),
-                                       grad(p[BA + 1], x - 1, y    , z - 1 )),
-                               lerp(u, grad(p[AB + 1], x    , y - 1, z - 1 ),
-                                       grad(p[BB + 1], x - 1, y - 1, z - 1 ))));
+
+        return this.lerp(w, this.lerp(v, this.lerp(u, this.grad(p[AA    ], x    , y    , z     ),
+                                                      this.grad(p[BA    ], x - 1, y    , z     )),
+                                         this.lerp(u, this.grad(p[AB    ], x    , y - 1, z     ),
+                                                      this.grad(p[BB    ], x - 1, y - 1, z     ))),
+                            this.lerp(v, this.lerp(u, this.grad(p[AA + 1], x    , y    , z - 1 ),
+                                                      this.grad(p[BA + 1], x - 1, y    , z - 1 )),
+                                         this.lerp(u, this.grad(p[AB + 1], x    , y - 1, z - 1 ),
+                                                      this.grad(p[BB + 1], x - 1, y - 1, z - 1 ))));
     }
 
-    private static fade = (t: number): number => {
+    private fade = (t: number): number => {
         
         return t * t * t * (t * (t * 6 - 15) + 10);
     }
 
-    private static lerp = (t: number, a: number, b: number): number => {
+    private lerp = (t: number, a: number, b: number): number => {
 
         return a + t * (b - a);
     }
 
-    private static grad = (hash: number, x: number, y: number, z: number): number => {
+    private grad = (hash: number, x: number, y: number, z: number): number => {
 
         let h: number = hash & 15,
             u: number = h < 8 ? x : y,
--- README.md
+++ README.md
@@ -4,8 +4,27 @@ Typescript reference implementation of improved noise.
 
 ## Usage
 
+Use the normal class.
+
 ```ts
 import { Perlin } from "./Perlin";
+
+let perlin: Perlin = new Perlin();
+
+// 1D Perlin Noise
+perlin.noise(x);
+
+// 2D Perlin Noise
+perlin.noise(x, y);
+
+// 3D Perlin Noise
+perlin.noise(x, y, z);
+```
+
+Or use the static class.
+
+```ts
+import { Perlin } from "./static/Perlin";
 
 // 1D Perlin Noise
 Perlin.noise(x);
@@ -16,3 +35,5 @@ Perlin.noise(x, y);
 // 3D Perlin Noise
 Perlin.noise(x, y, z);
 ```
+
+Personally, I prefer the static class, but it's completely up to you. They both do the same.
